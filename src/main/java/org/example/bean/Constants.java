20a3011d948368c5d848eef4fa38c2e0274663e9
LXT <2284650586@qq.com>
2023-12-04 23:43:09 +0800

    build field detail

--- src/main/java/org/example/bean/Constants.java
+++ src/main/java/org/example/bean/Constants.java
@@ -11,4 +11,13 @@ public class Constants {
         SUFFIX_BEAN_PARAM = PropertiesUtil.getString("suffix.bean.param");
     }
 
+    //    SQL类型到JAVA类型映射
+    public final static String[] SQL_DATE_TIME_TYPES = new String[]{"datetime", "timestamp"};
+    public final static String[] SQL_DATE_TYPES = new String[]{"date"};
+
+    public final static String[] SQL_DECIMAL_TYPE = new String[]{"decimal", "double", "float"};
+    public final static String[] SQL_STRING_TYPE = new String[]{"char", "varchar", "text", "mediumtext", "longtext"};
+    public final static String[] SQL_INTEGER_TYPE = new String[]{"int", "tinyint"};
+    public final static String[] SQL_LONG_TYPE = new String[]{"bigint"};
+
 }
--- src/main/java/org/example/builder/BuildTable.java
+++ src/main/java/org/example/builder/BuildTable.java
@@ -1,13 +1,14 @@
 package org.example.builder;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.example.bean.Constants;
+import org.example.bean.FieldInfo;
 import org.example.bean.TableInfo;
 import org.example.utils.PropertiesUtil;
 import org.example.utils.StringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.reflect.Field;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +18,7 @@ public class BuildTable {
     private static Connection conn = null;
     private static final Logger logger = LoggerFactory.getLogger(BuildTable.class);
     private static final String SQL_SHOW_TABLE_STATUS = "show table status";
+    private static final String SQL_SHOW_TABLE_FIELDS = "show full fields from %s";
 
     static {
         String driverName = PropertiesUtil.getString("db.driver.name");
@@ -47,6 +49,7 @@ public class BuildTable {
 
                 TableInfo tableInfo = new TableInfo();
 
+
                 tableInfo.setTableName(tableName);
 
                 String beanName = tableName;
@@ -62,12 +65,11 @@ public class BuildTable {
                 logger.info("表:{}, 备注:{}, javaBean:{}, javaParaBean:{}",
                         tableInfo.getTableName(), tableInfo.getComment(), tableInfo.getBeanName(), tableInfo.getBeanParamName());
 
-
+                readFieldInfo(tableInfo);
                 tableInfoList.add(tableInfo);
 
+
             }
-
-
         } catch (Exception e) {
             logger.error("读取表失败");
         } finally {
@@ -95,6 +97,82 @@ public class BuildTable {
         }
     }
 
+    private static List<FieldInfo> readFieldInfo(TableInfo tableInfo) {
+        PreparedStatement ps = null;
+        ResultSet fieldResult = null;
+
+        List<FieldInfo> fieldInfoList = new ArrayList<>();
+
+        try {
+            ps = conn.prepareStatement(String.format(SQL_SHOW_TABLE_FIELDS, tableInfo.getTableName()));
+            fieldResult = ps.executeQuery();
+
+            boolean haveDate = false;
+            boolean haveBigDecimal = false;
+
+            while (fieldResult.next()) {
+                String field = fieldResult.getString("field");
+                String type = fieldResult.getString("type");
+                String extra = fieldResult.getString("extra");
+                String comment = fieldResult.getString("comment");
+                if (type.indexOf("(") > 0) {
+                    type = type.substring(0, type.indexOf("("));
+                }
+
+                String propertyName = processField(field, false);
+
+                FieldInfo fieldInfo = new FieldInfo();
+
+                fieldInfo.setFieldName(field);
+                fieldInfo.setComment(comment);
+                fieldInfo.setSqlType(type);
+                fieldInfo.setAutoIncrement("auto_increment".equalsIgnoreCase(extra));
+                fieldInfo.setPropertyName(propertyName);
+                fieldInfo.setJavaType(processJavaType(type));
+
+
+                if(ArrayUtils.contains(Constants.SQL_DATE_TYPES,type) || ArrayUtils.contains(Constants.SQL_DATE_TIME_TYPES,type)){
+                    haveDate = true;
+                }
+                if(ArrayUtils.contains(Constants.SQL_DECIMAL_TYPE,type)){
+                    haveBigDecimal = true;
+                }
+
+                tableInfo.setFieldInfoList(fieldInfoList);
+
+
+                tableInfo.setHaveBigDecimal(haveBigDecimal);
+                tableInfo.setHaveDate(haveDate);
+                tableInfo.setHaveDateTime(haveDate);
+
+                fieldInfoList.add(fieldInfo);
+
+
+                logger.info("field:{}, type:{}, extre:{}, comment:{}, propertyName:{}, javaType:{}",
+                        field, type, extra, comment, propertyName, processJavaType(type));
+            }
+        } catch (Exception e) {
+            logger.error("读取表失败");
+        } finally {
+            if (fieldResult != null) {
+                try {
+                    fieldResult.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return fieldInfoList;
+    }
+
     private static String processField(String field, Boolean upCaseFirstLetter) {
         StringBuffer sb = new StringBuffer();
         String[] fields = field.split("_");
@@ -108,9 +186,20 @@ public class BuildTable {
         return sb.toString();
     }
 
-
-    public static void main(String[] args) {
-        System.out.printf(processField("info_code", true));
+    private static String processJavaType(String type) {
+        if (ArrayUtils.contains(Constants.SQL_DATE_TYPES, type) || ArrayUtils.contains(Constants.SQL_DATE_TIME_TYPES, type)) {
+            return "Date";
+        } else if (ArrayUtils.contains(Constants.SQL_DECIMAL_TYPE, type)) {
+            return "BigDecimal";
+        } else if (ArrayUtils.contains(Constants.SQL_STRING_TYPE, type)) {
+            return "String";
+        } else if (ArrayUtils.contains(Constants.SQL_INTEGER_TYPE, type)) {
+            return "Integer";
+        } else if (ArrayUtils.contains(Constants.SQL_LONG_TYPE, type)) {
+            return "Long";
+        } else {
+            throw new RuntimeException("无法识别的类型:" + type);
+        }
     }
 
 }
